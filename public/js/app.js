17c1d563a61187459f3c53fd841cbb0f18b3c6da
Praveen Singh <praveen@etech-services.com>
2020-03-21 16:49:17 +0530

    Remove unnecessary console.log call

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,3 @@
-console.log('Hello From Javascript...');
-
 const wheatherForm = document.querySelector('form');
 const search = document.querySelector('input');
 const messageOne = document.querySelector('#message-1');
